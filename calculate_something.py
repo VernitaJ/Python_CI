c73771d9a5bee5a0789fce299a8f2b08ed890f5a
VernitaJ <gusgouve@student.gu.se>
2022-11-09 08:30:55 +0100

    simple calculations added

---
+++ calculate_something.py
@@ -0,0 +1,13 @@
+#Basic calculator
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+def divide(first_term, second_term):
+    return first_term / second_term
+
+def multiply(first_term, second_term):
+    return first_term * second_term
